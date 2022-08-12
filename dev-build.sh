474f2fd79b46351fb5759cef1a9dd70a86d21888
Antonio Broughton <antonio@flerwin.com>
2022-08-12 23:06:21 +1200

    some playing around with page things

--- dev-build.sh
+++ dev-build.sh
@@ -2,5 +2,6 @@
 
 ./build.sh
 
+rm -rf ./out
 files=(main.min.js index.html)
 mkdir -p ./out && for f in "${files[@]}"; do cp $f $_; done
--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,10 @@ module Main exposing (main)
 
 import Browser exposing (Document, UrlRequest)
 import Browser.Navigation as Nav
-import Html exposing (div, text)
+import Html exposing (a, div, text)
+import Html.Attributes exposing (href)
 import Url exposing (Url)
+import Url.Parser exposing ((</>))
 
 
 main =
@@ -17,8 +19,14 @@ main =
         }
 
 
+type Route
+    = Home
+    | About
+
+
 type alias Model =
     { key : Nav.Key
+    , route : Maybe Route
     }
 
 
@@ -28,9 +36,17 @@ type Msg
     | UrlChanged Url
 
 
+urlParser : Url.Parser.Parser (Route -> a) a
+urlParser =
+    Url.Parser.oneOf
+        [ Url.Parser.map Home <| Url.Parser.top
+        , Url.Parser.map About <| Url.Parser.s "about"
+        ]
+
+
 init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-    ( { key = key }, Cmd.none )
+    ( { key = key, route = Url.Parser.parse urlParser url }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -38,11 +54,33 @@ subscriptions mode =
     Sub.none
 
 
+getTitle : Maybe Route -> String
+getTitle name =
+    let
+        pageName =
+            case name of
+                Just Home ->
+                    "Home"
+
+                Just About ->
+                    "About"
+
+                Nothing ->
+                    "Invalid"
+    in
+    "Flerwin - [" ++ pageName ++ "]"
+
+
 view : Model -> Document Msg
 view model =
-    { title = "Some title"
+    { title = getTitle model.route
     , body =
-        [ div [] [ text "Some text" ] ]
+        [ div []
+            [ div [] [ text "Some text" ]
+            , div [] [ text <| getTitle model.route ]
+            , div [] [ a [ href "/about" ] [ text "/about" ] ]
+            ]
+        ]
     }
 
 
@@ -53,7 +91,12 @@ update msg model =
             ( model, Cmd.none )
 
         LinkClicked urlRequest ->
-            ( model, Cmd.none )
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key <| Url.toString url )
+
+                Browser.External url ->
+                    ( model, Nav.load url )
 
         UrlChanged url ->
-            ( model, Cmd.none )
+            ( { model | route = Url.Parser.parse urlParser url }, Cmd.none )
